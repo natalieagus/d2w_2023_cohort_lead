37c087743106f9047866ef9454b6684f82aa15fa
Natalie Agus <nn88blue@gmail.com>
2023-11-10 12:36:41 +0800

    feat: implemented cs_6

--- week9/source/cs_6.py
+++ week9/source/cs_6.py
@@ -1,5 +1,8 @@
-import numpy as np 
+import numpy as np
+
 
 def mean_squared_error(target, pred):
-    ## TODO 
-    pass 
+    error = target - pred  # find the diff between y and yhat, this is our error
+    sq_error = np.matmul(error.T, error)  # dont forget, this a matrix with 1 element
+    # get the mean of the sq error and return
+    return (1 / target.shape[0]) * sq_error[0][0]
