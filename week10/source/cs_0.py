f54785e27af824b2cf665e9e7cbbb5757ee8eac9
Natalie Agus <nn88blue@gmail.com>
2023-11-15 12:48:21 +0800

    fix: fix missing import OS in cs0

--- week10/source/cs_0.py
+++ week10/source/cs_0.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import os
 
 
 def normalize_z(dfin, columns_means=None, columns_stds=None):
--- week10/tests/test_cs_3.py
+++ week10/tests/test_cs_3.py
@@ -1,23 +1,25 @@
-import numpy as np 
-from source.cs_3 import gradient_descent_logreg 
-try: 
+import numpy as np
+from source.cs_3 import gradient_descent_logreg
+
+try:
     from source.cs_0 import feature, target
 except:
     print("cs_0 not implemented yet")
-    
+
 import matplotlib.pyplot as plt
-import pytest 
+import pytest
+
 
 def test_cs_3():
     iterations = 1500
     alpha = 0.01
-    beta = np.zeros((2,1))
+    beta = np.zeros((2, 1))
     beta, J_storage = gradient_descent_logreg(feature, target, beta, alpha, iterations)
 
     assert beta.shape == (2, 1)
     assert np.isclose(beta[0][0], -0.56630)
     assert np.isclose(beta[1][0], 1.93764)
 
-    if (pytest.plot_output):
+    if pytest.plot_output:
         plt.plot(J_storage)
         plt.show()
