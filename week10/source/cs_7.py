b5a57cc9ebda9894dd91146c60caf1725b43f5c9
Natalie Agus <nn88blue@gmail.com>
2023-11-15 10:36:33 +0800

    feat: finish cs7

--- week10/source/cs_7.py
+++ week10/source/cs_7.py
@@ -1,5 +1,12 @@
-import numpy as np 
+import numpy as np
+
 
 def calc_accuracy(cm):
-    ## TODO 
-    pass  
+    negneg, pospos, negpos, posneg = cm[(0, 0)], cm[(1, 1)], cm[(0, 1)], cm[(1, 0)]
+
+    return {
+        "accuracy": (negneg + pospos) / np.sum(list(cm.values())),
+        "sensitivity": pospos / (pospos + posneg),
+        "specificity": negneg / (negneg + negpos),
+        "precision": pospos / (pospos + negpos),
+    }
