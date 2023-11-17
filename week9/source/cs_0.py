73f7dd3cb218db7a6997a3244e978e4783c73c92
Natalie Agus <nn88blue@gmail.com>
2023-11-17 13:06:37 +0800

    fix: use is instead of None

--- week9/source/cs_0.py
+++ week9/source/cs_0.py
@@ -2,11 +2,11 @@
 # receive two opt parameters: columns_means and columns_stds
 def normalize_z(dfin, columns_means=None, columns_stds=None):
     # check if opt args are given 
-    if columns_means == None:
+    if columns_means is None:
         # take the mean per column 
         # if i have 3 cols: i take the mean of each cols separately
         columns_means = dfin.mean(axis=0)
-    if columns_stds == None: 
+    if columns_stds is None: 
         # columns_stds is given as opt arg because if dfin only contains 1 sample (1 row), then dfin.std(axis=0) will be 0 and dfout will be error
         columns_stds = dfin.std(axis=0)
     dfout = (dfin - columns_means) / columns_stds
